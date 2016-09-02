9db17232537a0fe88ce5bfcab1f32c7e658d9a07
tobie_eniafe <tobisamuel88@gmail.com>
2016-09-02 15:25:32 +0100

    admin.php UI done, upload still not working

--- admin.php
+++ admin.php
@@ -60,17 +60,101 @@ if ($uploadOk == 0) {
          
 	</head>
 <body>
-	<h1>Welcome Admin</h1>
-	<a href="logout.php">Logout</a>
+	
+	
 
-	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
-	    Select file to upload:
-	    <input type="file" name="fileToUpload" id="fileToUpload">
-	    <input type="submit" value="Upload file" name="submit">
-	</form>
-	<?php 
-	 echo $notif;
-	 ?>
+	
+
+<!-- Admin homepage -->
+<div data-role="page" id="adminhome">
+  <div data-role="panel" id="myPanel" data-swipe-close="true" data-position-fixed="true" data-theme="a"> 
+    <h2>Menu</h2>
+
+	    <ul data-role="listview" data-inset="true">    
+	      <li><a href="#upload" >Upload Files</a></li>
+	      <li><a href="#">Manage Categories</a></li>
+	      <li><a href="#">Settings</a></li>
+	    </ul>
+
+  </div> 
+
+  <div data-role="header"  data-theme="b">
+    <a href="#myPanel"data-role="button" data-icon="bullets">Menu</a>
+    <h1>PlayMobile - Admin</h1>
+    <a href="logout.php" data-role="button" data-icon="power" class="ui-btn-right">Logout</a>
+  </div>
+
+  <div data-role="main" class="ui-content">
+    <h1>Welcome Admin</h1>
+    
+  </div>
+  <!-- Footer start -->
+  <div data-role="footer" data-position="fixed" data-id="footernav">
+		<div data-role="navbar">
+			<ul>
+				<li><a href="index.php" data-icon='home' data-role='button'>Home</a></li>
+				<li><a href="playerlist.php" rel="external" id="listTracks" data-icon='audio' data-role='button'>Play All</a></li>
+				<li><a href="#" data-icon='user' data-role='button'>Contact</a></li>
+			</ul>
+		</div>
+	</div>
+	<!-- Footer end -->
+
+  
+</div> 
+
+
+<!-- Upload page -->
+
+<div data-role="page" id="upload">
+  <div data-role="panel" id="myPanel" data-swipe-close="true" data-position-fixed="true" data-theme="a"> 
+    
+
+	    <ul data-role="listview" data-inset="true">    
+	      <li><a href="#upload">Upload Files</a></li>
+	      <li><a href="#">Manage Categories</a></li>
+	      <li><a href="#">Settings</a></li>
+	    </ul>
+
+  </div> 
+
+  <div data-role="header"  data-theme="b">
+  	<a href="#myPanel"data-role="button" data-icon="bullets">Menu</a>
+    <h1>PlayMobile - Admin</h1>
+    <a href="logout.php" data-role="button" data-icon="power" class="ui-btn-right">Logout</a>
+  </div>
+
+  <div data-role="main" class="ui-content">
+  		<!-- Upload Form -->
+		<div data-role="main" class="ui-content">
+		    <div data-role="controlgroup" data-type="horizontal">
+		    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" enctype="multipart/form-data">
+				<input type="file" name="fileToUpload" id="fileToUpload">  
+				<input  type="submit" href="#" class="ui-btn" value="Upload file" name="submit" >
+			</form>
+			<?php 
+    		echo $notif;
+    		?>
+		    </div>
+
+	  
+		</div> 
+	</div>
+
+	<!-- Footer start -->
+	<div data-role="footer" data-position="fixed" data-id="footernav">
+		<div data-role="navbar">
+			<ul>
+				<li><a href="index.php" data-icon='home' data-role='button'>Home</a></li>
+				<li><a href="playerlist.php" rel="external" id="listTracks" data-icon='audio' data-role='button'>Play All</a></li>
+				<li><a href="#" data-icon='user' data-role='button'>Contact</a></li>
+			</ul>
+		</div>
+	</div>
+	<!-- Footer end -->
+</div>
+
+
 
 </body>
 </html>
